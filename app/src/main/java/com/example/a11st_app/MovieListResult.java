40fa9fd143fcc57e8bf461ad6954714a45aae1a3
sddalswn <sddalswn@naver.com>
2022-05-24 16:15:03 +0900

    영화 정보의 수 추가

---
+++ app/src/main/java/com/example/a11st_app/MovieListResult.java
@@ -0,0 +1,11 @@
+package com.example.a11st_app;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showEange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
